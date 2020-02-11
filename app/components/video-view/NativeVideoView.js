0e442453282b65b8dbf61f1492d2e9582611a5b0
AndreyKapitonov <dj-on-ik@mail.ru>
2020-02-11 15:58:20 +0300

    Video player with Google IMA SDK

--- app/components/video-view/NativeVideoView.js
+++ app/components/video-view/NativeVideoView.js
@@ -11,6 +11,11 @@ import {
 import Video from 'react-native-video';
 import Orientation from 'react-native-orientation';
 
+const adTagUrl = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/"
+    + "ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp"
+    + "&output=vmap&unviewed_position_start=1&cust_params=deployment%3Ddevsite"
+    + "%26sample_ar%3Dpremidpost&cmsid=496&vid=short_onecue&correlator=";
+
 const NativeVideoView = ({ onFullscreen, ...props }) => {
 
     const [ rate, setRate ] = useState(1);
@@ -135,6 +140,7 @@ const NativeVideoView = ({ onFullscreen, ...props }) => {
                     onAudioFocusChanged={onAudioFocusChanged}
                     repeat={false}
                     fullscreen={true}
+                    adTagUrl={adTagUrl}
                 />
             </TouchableOpacity>
 
